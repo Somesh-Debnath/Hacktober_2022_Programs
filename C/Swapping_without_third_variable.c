ef6ed61ce5edb97016e3390b0df5b70e40dcca46
Guillotine189 <114574134+Guillotine189@users.noreply.github.com>
2022-10-16 10:11:22 +0530

    Made and modified a file

---
+++ C/Swapping_without_third_variable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int a,b;
+    printf("Enter Value of A\n>>");
+    scanf("%d", &a);
+
+    printf("Enter Value of B\n>>");
+    scanf("%d",&b);
+
+    printf("As of right now the values of A and B are %d and %d respectively\n", a,b);
+
+    a = a + b - (b = a);
+
+    printf("Now after switching the values without using a third variable the new values of A and B are %d and  %d respectively.\n", a,b);
+    
+    return 0;
+
+}
